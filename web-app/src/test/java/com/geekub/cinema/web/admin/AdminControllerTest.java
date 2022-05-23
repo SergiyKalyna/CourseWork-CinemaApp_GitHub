8062d401a15cdd790bcfc26c88ceeebb35d4bda6
SergiyKalyna <seryjk452@gmail.com>
2022-05-23 21:48:28 +0300

    CW: Changed AdminController and HallController cover by tests.

--- web-app/src/test/java/com/geekub/cinema/web/admin/AdminControllerTest.java
+++ web-app/src/test/java/com/geekub/cinema/web/admin/AdminControllerTest.java
@@ -8,11 +8,14 @@ import com.geekhub.user.UserService;
 import com.geekhub.user.dto.UserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
@@ -26,6 +29,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@Sql(scripts = "classpath:schema.sql")
 @WithMockUser(username = "admin", roles = "ADMIN")
 class AdminControllerTest {
 
--- web-app/src/test/java/com/geekub/cinema/web/hall/HallControllerTest.java
+++ web-app/src/test/java/com/geekub/cinema/web/hall/HallControllerTest.java
@@ -6,12 +6,19 @@ import com.geekhub.cinemahall.CinemaHallService;
 import com.geekhub.cinemahall.dto.CinemaHallDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 import static org.mockito.Mockito.verify;
@@ -23,10 +30,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@Sql(scripts = "classpath:schema.sql")
 class HallControllerTest {
 
-    @Autowired
-    HallController hallController;
 
     @Autowired
     MockMvc mockMvc;
